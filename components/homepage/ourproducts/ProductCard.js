1b6cf5622afdc079de19ba58f0a872b9fb94f2fe
Okwor Chinedu <devplus001@gmail.com>
2024-02-18 12:54:02 +0100

    still working on the product page for gethired,

---
+++ components/homepage/ourproducts/ProductCard.js
@@ -0,0 +1,28 @@
+import ImageCont from '@/components/common/images'
+import style from './products.module.css'
+import { MdArrowForwardIos } from "react-icons/md";
+import Link from 'next/link';
+
+function ProductCard({img, alt, name, text, url}) {
+  return (
+    <div className={`${style.productCard} ${style.ActiveCard}`}>
+        <div className={style.imageCint}>
+            <div className={style.image}>
+                <ImageCont
+                    src={img}
+                    alt={alt}
+                    height={'12rem'}
+                    width={'100%'}/>
+                <p></p>
+            </div>
+        </div>
+        <p className={`${style.botton} ${style.name}`}>{name}</p>
+        <div className={style.text}>
+            <p>{text}</p>
+            <Link href={url} className={`${style.botton} ${style.botton1}`}><MdArrowForwardIos/></Link>
+        </div>
+    </div>
+  )
+}
+
+export default ProductCard
--- components/homepage/ourproducts/index.js
+++ components/homepage/ourproducts/index.js
@@ -1,11 +1,62 @@
-import React from 'react'
+import ProductCard from './ProductCard'
+import style from './products.module.css'
+//add a carousel slide to this and more importantly a scroll tab showing the length of scroll one has to do
 
 function index() {
+
+  const renderProducts = (products)=>{
+    if (products?.length > 4) 
+    return products.map((data, index)=><ProductCard
+      key={index}
+      img={data.img}
+      alt={data.alt}
+      name={data.name}
+      text={data.text}
+      url={data.url} />) 
+
+    return [...products.map((data, index)=><ProductCard
+      key={index}
+      img={data.img}
+      alt={data.alt}
+      name={data.name}
+      text={data.text}
+      url={data.url} />) 
+      ,
+      ...[1,2,3].map(data=><ComingSoonCard/>)]
+    
+  }
+  console.log()
+
   return (
-    <div>
-        our products
+    <div className={`${style.container} width`}>
+        <div className={style.inneContainer}>
+          <h1 className='title'> Our Products</h1>
+          <div className={style.productsContainer}> 
+            {renderProducts(productData)}
+          </div>
+        </div>
     </div>
   )
 }
 
 export default index
+
+
+export const ComingSoonCard = () =>{
+
+  return <div className={`${style.productCard} ${style.ComingSoonCard}`}>
+    <h1>coming soon</h1>
+  </div>
+}
+
+const productData = [
+  // The data displayed here is to be fecthed from our database for our products
+  // once the database is configured, we can start working on the fetchinf and appropriate rendering
+  {
+    img: '/images/gethired.png',
+    alt: 'gethired',
+    name: 'GetHired',
+    text: 'Effortlessly connect job seekers with employment opportunities or employers with top talent, all with just one click.',
+    url: '/'
+  }
+]
