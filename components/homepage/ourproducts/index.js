fe43399d5bccc022b52f2f50da37d7997bec11f3
Orjiene Kenechukwu <supersonicwisdom@gmail.com>
2024-03-01 15:57:58 +0100

    enabling integration of product rendering to the site

--- components/homepage/ourproducts/index.js
+++ components/homepage/ourproducts/index.js
@@ -1,91 +1,93 @@
-'use client'
-import ProductCard from './ProductCard'
-import style from './products.module.css'
-import { MdArrowForwardIos } from "react-icons/md";
-import { MdArrowBackIosNew } from "react-icons/md";
-import { useRef, useState } from 'react';
+'use client'; // Not strictly necessary in modern React, but can be kept for clarity
+
+import ProductCard from './ProductCard';
+import style from './products.module.css';
+import { MdArrowForwardIos, MdArrowBackIosNew } from 'react-icons/md';
+import { useRef, useState, useEffect } from 'react';
 import axios from 'axios';
-import { FaFacebook } from 'react-icons/fa6';
-const apiLink = 'https://api.solvexng.com/api/v1/products/'
+
+const apiLink = 'https://api.solvexng.com/api/v1/products/';
 
 function Index() {
-  const ref = useRef(null)
-  const [scrollPoint, setscrollPoint] = useState(0)
-  const [productData, setproductData] = useState([])
+  const ref = useRef(null);
+  const [scrollPoint, setScrollPoint] = useState(0);
+  const [productData, setProductData] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
 
-  const getData = async () => {
-    try {
-      const response = await axios.get(apiLink);
-      // am receiving a cores error from this pint of fetching data, I hope that logging into the resourse will allow it be fetched
-      console.log(response.message)
-      setproductData(response.message)
-    } catch (error) {
-      const errormsg= "Error fetching products, refresh your browser to try again"
-      
-    }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(apiLink);
+        setProductData(response.data);
+      } catch (error) {
+        setErrorMessage('Error fetching products. Please try again later.');
+      }
+    };
+
+    fetchData();
+  }, []); 
+
+  const handleScroll = () => {
+    ref.current.scrollLeft = 550 * (scrollPoint - 1);
+    setScrollPoint(scrollPoint - 1);
   };
-  getData()
 
-  const handleScroll =()=>{
-    ref.current.scrollLeft = 550*(scrollPoint-1);
-    setscrollPoint(scrollPoint - 1)
-  }
-  const handleScrollforward =()=>{
-    ref.current.scrollLeft = 550*(scrollPoint+1);
-    setscrollPoint(scrollPoint + 1)
-  }
-
-  console.log(productData)
+  const handleScrollForward = () => {
+    ref.current.scrollLeft = 550 * (scrollPoint + 1);
+    setScrollPoint(scrollPoint + 1);
+  };
 
   return (
-    <div className={`${style.container} width`} id='product'>
-        <div className='relative'>
-          <div className={style.inneContainer}>
-            <h1 className='title'> Our Products</h1>
-            <div className={style.productsContainer} ref={ref}> 
-              {!productData.length && [...[1,2,3,4]].map((data, index)=><ComingSoonCard key={index}/>)}
-              {productData.length < 4 > 0 
-                && [...[...productData.map((data, index)=><ProductCard
-                  key={index+4}
-                  img={data.logo}
-                  alt={data.name}
-                  name={data.name}
-                  text={data.description}
-                  url={data.url} />) 
-                  ,
-                  ...[...new Array(4-productData.length)].map((data, index)=><ComingSoonCard key={index}/>)]]
-              }
-              {productData.length > 3 
-                && productData.map((data, index)=><ProductCard
-                  key={index}
-                  img={data.logo}
-                  alt={data.name}
-                  name={data.name}
-                  text={data.description}
-                  url={data.url} />) }
-
-            </div>
+    <div className={`${style.container} width`} id="product">
+      <div className="relative">
+        <div className={style.innerContainer}>
+          <h1 className="title">Our Products</h1>
+          <div className={style.productsContainer} ref={ref}>
+            {/* Check for error if there is any error */}
+            {errorMessage && <div className={style.error}>{errorMessage}</div>}
+            
+            {/* TODO add a display it will show if product not available */}
+            {/* TODO render image. Image rendering is still faulty */}
+            {/* Only render elements when productData is loaded */}
+            {productData.length > 0 && (
+              <>
+                {[...productData].map((data, index) => (
+                  <ProductCard
+                    key={index}
+                    img={data.logo}
+                    alt={data.name}
+                    name={data.name}
+                    text={data.description}
+                    url={data.url}
+                  />
+                ))}
+                {/* Handle cases where productData is less than 4 */}
+                {productData.length < 4 &&
+                  [...Array(4 - productData.length)].map((_, index) => (
+                    <ComingSoonCard key={index} />
+                  ))}
+              </>
+            )}
           </div>
-          {scrollPoint != 0 && <button className={style.leftbtn} onClick={()=>handleScroll()}><MdArrowBackIosNew/></button>}
-          <button className={style.rightbtn} onClick={()=>handleScrollforward()}><MdArrowForwardIos/></button>
         </div>
-
-        
+        {scrollPoint !== 0 && (
+          <button className={style.leftbtn} onClick={handleScroll}>
+            <MdArrowBackIosNew />
+          </button>
+        )}
+        <button className={style.rightbtn} onClick={handleScrollForward}>
+          <MdArrowForwardIos />
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Index
+export default Index;
 
-
-export const ComingSoonCard = () =>{
-
+export const ComingSoonCard = () => {
   return <div className={`${style.productCard} ${style.ComingSoonCard}`}>
     <h1>coming soon</h1>
-  </div>
-}
-
-
-
-
-
+  </div>;
+};
